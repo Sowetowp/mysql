e50f9b165114c5da44b10f19cdba1a385d69c1c6
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-28 15:48:01 +0100

    auto-commit

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USERNAME,
+    user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.DB_PORT
