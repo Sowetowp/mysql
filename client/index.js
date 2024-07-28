27f59ec6163ffb49831f2c14634eafefe4551b34
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-28 11:09:49 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -5,6 +5,6 @@ function loadHtmlTable(data) {
     const table = document.querySelector('table tbody')
     let tableHtml = ""
     if (data.length === 0) {
-table.innerHTML = "<tr><td class='no-data'></td></tr>"
+table.innerHTML = "<tr><td class='no-data' colSpan='5'></td></tr>"
     }
 }
