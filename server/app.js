d385f59ac4d940e73484be265d5514f83c847802
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-28 15:40:44 +0100

    auto-commit

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 dotenv.config()
 
-const 
+const dbService = require("./db.js")
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
