a453ab26cb65c846da15fd1816eddb52525c703a
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-28 19:16:09 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:5000/getAll')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => loadHtmlTable([]))
     
 })
 function loadHtmlTable(data) {
