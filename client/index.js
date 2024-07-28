d64382a668d2d8fa9de53a4038e0d57534b1e370
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-28 11:11:36 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -7,4 +7,4 @@ function loadHtmlTable(data) {
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>"
     }
-}<iframe width="633" height="351" src="https://www.youtube.com/embed/vrj9AohVhPA" title="Node.js, Express &amp; MySQL Tutorial - Build a Simple FullStack App" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+}
