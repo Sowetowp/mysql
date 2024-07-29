7dc163db117e15102f43f91337ed9d3f9e2e5d18
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 19:41:09 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -29,11 +29,12 @@ function insertRowIntoTable (data){
 function loadHtmlTable(data) {
     const table = document.querySelector('table tbody')
     console.log(data)
-    let tableHtml = ""
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>"
+        return
     }
+    let tableHtml = ""
     data.array.forEach(function({id, name, date_added}){
-        
+
     });
 }
