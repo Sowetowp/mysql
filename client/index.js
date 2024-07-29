484d827d0d919f9cb4ec719bf4daa137c55a1124
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 21:41:48 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -38,7 +38,7 @@ function insertRowIntoTable (data){
         table.innerHTML = tableHtml
     }else{
         const newRow = table.insertRow()
-        newRow.innerHTML = 
+        newRow.innerHTML = tableHtml
     }
 }
 
