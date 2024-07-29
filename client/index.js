62dca4744bfb3b2ffb871d73e82e0973a860c87c
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 22:12:20 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -23,6 +23,7 @@ addBtn.onclick = function () {
 }
 
 function insertRowIntoTable(data) {
+    console.log(data)
     const table = document.querySelector('table tbody')
     const isTableData = document.querySelector(".no-data")
     let tableHtml = "<tr>"
