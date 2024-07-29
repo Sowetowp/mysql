09877001f02dc0efd9226ed070b79649855975b2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 22:25:56 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const addBtn = document.querySelector("#add-name-btn")
-const deleteBtn = document.querySelector("#add-name-btn")
 addBtn.onclick = function () {
     const nameInput = document.querySelector("#nameInput")
     const name = nameInput.value
