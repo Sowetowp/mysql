0254d3725f88950e4f056478daa3370813b1d4d3
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-30 00:40:16 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -36,7 +36,7 @@ function handleEditRow(id) {
 
 updateBtn.onclick = function () {
     const updateNameInput = document.querySelector("#update-name-input")
-    console.log(updateNameInput.dataset.id)
+    console.log(updateBtn.dataset.id)
     fetch('http://localhost:5000/update', {
         method: "PATCH",
         headers: {
