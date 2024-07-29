60ce768be5dd7bf70770bd94fa5eb082b788c340
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-30 00:11:19 +0100

    auto-commit

--- server/db.js
+++ server/db.js
@@ -86,7 +86,7 @@ class DbService {
         try {
             id = parseInt(id, 10)
             const response = await new Promise((resolve, reject) => {
-                const query = "DELETE FROM names WHERE id = ?"
+                const query = ""
                 connection.query(query, [id], (err, result) => {
                     if (err) {
                         console.error('Error executing query:', err.message);
