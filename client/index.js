83dd7b44cf4be67aa976058daefd89bd2f5a5afe
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 02:04:18 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -9,6 +9,7 @@ const addBtn = document.querySelector("#add-name-btn")
 addBtn.onclick = function (){
     const nameInput = document.querySelector("#nameInput")
     const name = nameInput.value
+    nameInput.value = ""
 }
 
 function loadHtmlTable(data) {
