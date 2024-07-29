7ac189caf079341055240291e45af9bf4a1010c7
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 21:55:35 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -31,7 +31,7 @@ function insertRowIntoTable(data) {
             if (keys === "dateAdded") {
                 data[keys] = new Date(data[keys].toLocaleString())
             }
-            tableHtml += 
+            tableHtml += `<td></td>`
         }
     }
     data.forEach(function ({ id, name, dateAdded }) {
