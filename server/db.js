bc57372e1f9967ef7c5acb30c677177bcfd4b1da
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 19:03:27 +0100

    auto-commit

--- server/db.js
+++ server/db.js
@@ -42,7 +42,7 @@ class DbService {
         try {
             const dateAdded = new Date();
             const insertId = await new Promise((resolve, reject) => {
-                const query = "INSERT INTO names (name, date_added) VALUES (?,?);"
+                const query = "ALTER TABLE names MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT;"
                 connection.query(query, [name, dateAdded], (err, result) => {
                     if (err) {
                         console.error('Error executing query:', err.message);
