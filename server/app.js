4e165019a68d3ffcf101a9f921aaf6bf2c3837c7
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-30 00:14:11 +0100

    auto-commit

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.get("/getAll", (req, res) => {
 app.patch("/update", (req, res) => {
     const {id, name} = req.body
     let db = dbService.getDbServiceInstance()
-    const result = db.deleteRow(id)
+    const result = db.updateRow(id)
     result
         .then(data => res.json({ success: data }))
         .catch(err => console.log(err))
