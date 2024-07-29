ad53c8f6cd82d2403830902270b2e68d836f5891
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-29 23:05:46 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.querySelector("table tbody").addEventListener("click", function (event) {
     if (event.target.className === "delete-row-btn") {
-        console.log(event.target)
+        console.log(event.target.dataSet)
     }
 })
 
