04b062f25eae07adb15951a0b3e242d481449f45
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 19:22:51 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -63,6 +63,7 @@ updateBtn.onclick = function () {
 }
 
 const addBtn = document.querySelector("#add-name-btn")
+console.log(addBtn)
 addBtn.onclick = function () {
     const nameInput = document.querySelector("#nameInput")
     const secretInput = document.querySelector("#secretInput")
