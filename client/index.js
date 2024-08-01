8e28b9ac8932b08aec04909769e56bdf962256d0
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 21:50:57 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -48,7 +48,7 @@ function handleEditRow(id) {
 updateBtn.onclick = function () {
     const updateNameInput = document.querySelector("#update-name-input")
     const updateCommentInput = document.querySelector("#update-comment-input")
-    updateCommentInput.v
+    updateCommentInput.value = 
     if (updateNameInput.value.trim() === "" || updateCommentInput.value.trim() === "") {
         return alert("Kindly fill all fields")
     }
