7a36638a2a4d6f5a1faa38bbf912205f65a5f2f7
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 19:11:47 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -77,7 +77,7 @@ addBtn.onclick = function () {
             "Content-type": 'application/json'
         },
         method: "POST",
-        body: JSON.stringify({ name: name, secret })
+        body: JSON.stringify({ name: name, secret: secret, comment:comment })
     })
         .then(res => res.json())
         .then(data => insertRowIntoTable(data["data"]))
