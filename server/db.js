9a54aeb49f6d9c99c85842019a9161362f39ace4
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 20:56:01 +0100

    auto-commit

--- server/db.js
+++ server/db.js
@@ -88,7 +88,7 @@ class DbService {
         try {
             id = parseInt(id, 10)
             const response = await new Promise((resolve, reject) => {
-                const query = "UPDATE names SET name = ? WHERE id = ?"
+                const query = "UPDATE names SET name = ? set comment = ? WHERE id = ?"
                 connection.query(query, [name, id], (err, result) => {
                     if (err) {
                         console.error('Error executing query:', err.message);
