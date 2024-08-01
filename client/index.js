912f7b49212973ff3a13082aaabc96a4babbafbb
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 21:47:35 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -40,7 +40,7 @@ function handleEditRow(id) {
     const updateSection = document.querySelector("#update-row")
     updateSection.hidden = false
     updateSection.classList.add("flex")
-    console.log(document.querySelector("#update-row-btn").dataset)
+    console.log(document.querySelector("#update-row-btn").dataset.id)
     document.querySelector("#update-row-btn").dataset.id = id
 }
 
