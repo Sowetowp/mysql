0ab7a5f33639feba53fa891faf6d4c544b6f8163
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 22:08:38 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -125,7 +125,7 @@ function insertRowIntoTable(data) {
     `
     tableHtml += `
         <td data-id=${data.id} class="py-3 delete-row-btn px-3 text-xs">
-            <svg viewBox="0 0 448 512" class="w-3 m-auto fill-red-400">
+            <svg fill=#f87171 viewBox="0 0 448 512" class="w-3 m-auto fill-red-400">
                 <path
                     d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
                 </path>
