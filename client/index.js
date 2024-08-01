be434bb28b448d8447ceddfdf832796b3ad0c997
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 20:30:34 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -94,8 +94,8 @@ function insertRowIntoTable(data) {
     const isTableData = document.querySelector(".no-data")
 
     let tableHtml = ""
-    // tableHtml += `<tr class="border-b">`
-    tableHtml += `<tr class="border-b"><td class="py-3 px-3 text-xs text-gray-300 text-center">${data.name}</td>`
+    t = `<tr class="border-b">`
+    tableHtml += `<td class="py-3 px-3 text-xs text-gray-300 text-center">${data.name}</td>`
     tableHtml += `<td class="py-3 px-3 text-xs text-white text-center">${data.comment}</td>`
     tableHtml += `<td class="py-3 px-3 text-xs text-gray-300 text-center">${new Date(data.dateAdded).toLocaleString()}</td>`
     // console.log(data)
@@ -123,11 +123,11 @@ function insertRowIntoTable(data) {
                     d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
                 </path>
             </svg>
-        </td></tr>
+        </td>
     `
     // tableHtml += `<td><button class='delete-row-btn' data-id=${data.id}>delete</button></td>`
     // tableHtml += `<td><button class='edit-row-btn' data-id=${data.id}>edit</button></td>`
-    // tableHtml += "</tr>"
+    tableHtml += "</tr>"
 
     if (isTableData) {
         table.innerHTML = tableHtml
