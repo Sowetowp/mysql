ef6a53a5491b8883e120001934b95d5189a2bddd
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 19:45:42 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -91,7 +91,7 @@ function insertRowIntoTable(data) {
     const table = document.querySelector('table tbody')
     const isTableData = document.querySelector(".no-data")
     let tableHtml = `<tr class="border-b">`
-    
+    tableHtml += `<td class="py-3 px-3 text-xs text-gray-300 text-center">${data[keys]}</td>`
     console.log(data)
     for (var keys in data) {
         if (data.hasOwnProperty(keys)) {
