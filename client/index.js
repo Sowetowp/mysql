a139d4f1e85ad5e1c54b1228fba3ab59a91187a9
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-01 22:18:07 +0100

    auto-commit

--- client/index.js
+++ client/index.js
@@ -108,9 +108,9 @@ function insertRowIntoTable(data) {
     const isTableData = document.querySelector(".no-data")
 
     let tableHtml = `<tr class="border-b">`
-    tableHtml += `<td class="py-3 px-3 text-xs text-gray-300 text-center">${data.name}</td>`
+    tableHtml += `<td class="py-3 px-3 tt text-xs text-gray-300 text-center">${data.name}</td>`
     tableHtml += `<td class="py-3 px-3 text-xs text-white text-center">${data.comment}</td>`
-    tableHtml += `<td class="py-3 px-3 text-xs text-gray-300 text-center">${new Date(data.dateAdded).toLocaleString()}</td>`
+    tableHtml += `<td class="py-3 px-3 tt text-xs text-gray-300 text-center">${new Date(data.dateAdded).toLocaleString()}</td>`
 
     tableHtml += `
         <td class="py-3 px-3 text-xs">
