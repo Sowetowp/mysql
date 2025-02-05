b135c25aad10d55df9c789e27cd9376bbba75304
Abdulmalik <ayodejiamzat@gmail.com>
2025-02-05 21:36:03 +0100

    f

--- server/db.js
+++ server/db.js
@@ -9,7 +9,10 @@ const pool = mysql.createPool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: { 
+        rejectUnauthorized: true // Ensures a secure connection
+    }
 });
 
 class DbService {
